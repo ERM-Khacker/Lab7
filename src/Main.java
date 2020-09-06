08461a0d50d24165acbd0e22f0862c36ca4c31d9
Umar Khalilov <ERMASTER100@gmail.com>
2020-09-06 22:23:38 +0300

    Lab 7 and 8 Correct

--- src/Main.java
+++ src/Main.java
@@ -41,143 +41,116 @@
 
         1.2 Вызвать методы в тестовом классе (Main)
 
-        1.3 Перегрузить метод водном из классов. Например, метод, который перелистывает на
+        1.3 Перегрузить метод в одном из классов. Например, метод, который перелистывает на
         определенную страницу перегрузить методом, который перелистывает на
         определенную главу.
 
         1.4 Создать статический метод в любом из классов. Например, создать статический метод
         в классе «Отделение почты», который будет рассчитывать объемный вес посылки, в
         зависимости от переданных параметров.*/
+
 public class Main {
     public static void main(String[] args) {
        /* Books book = new Books();
         book.name = "Dark Tower - 7 books";
-        book.name2 = "It";
-        book.name3 = "The Shining";
-        book.pagesDark = 100000;
-        book.pagesIt = 1138;
-        book.pagesShining = 447;
-        System.out.println(book.author);
-        System.out.println(book.name + " " + book.pagesDark);
-        System.out.println(book.name2 + " " + book.pagesIt);
-        System.out.println(book.name3 + " " + book.pagesShining);
+
         PostOffice post = new PostOffice();
-        post.name = "Новая почта";
-        post.name2 = "Укрпочта";
-        post.name3 = "Justin";
-        post.clientsNewPost = 1000000;
-        post.clientsUkr = 500000;
-        post.clientsJustin = 200000;
-        System.out.println(post.name + " " + post.clientsNewPost);
-        System.out.println(post.name2 + " " + post.clientsUkr);
-        System.out.println(post.name3 + " " + post.clientsJustin);
+
         Market supMarket = new Market();
-        supMarket.name = "Сільпо";
-        supMarket.name2 = "Varus";
-        supMarket.name3 = "ATB";
-        supMarket.clientsVarus = 2000;
-        supMarket.clientsATB = 100000;
-        System.out.println(supMarket.name + " " + supMarket.CLIENTS_SILPO);
-        System.out.println(supMarket.name2 + " " + supMarket.clientsVarus);
-        System.out.println(supMarket.name3 + " " + supMarket.clientsATB);*/
+        */
         Books books = new Books();
-        books.setName("Dark Tower - 7 books");
-        books.setName2("It");
-        books.setName3("The Shining");
-        books.setPagesDark(100000);
-        books.setPagesShining(447);
-        books.setPagesIt(1138);
-        System.out.println(books.getAuthor());
-        System.out.println(books.getName());
-        System.out.println(books.getPagesDark());
-        System.out.println(books.getName2());
-        System.out.println(books.getPagesIt());
-        System.out.println(books.getName3());
-        System.out.println(books.getPagesShining());
+        books.setName("The Dark Tower");
+        Books.setPages(10000);
+        System.out.println(Books.getAUTHOR());
+        System.out.println(books.getName() + ", " + Books.getPages());
+        Books books1 = new Books();
+        books1.setName("It");
+        Books.setPages(1138);
+        System.out.println(books1.getName() + ", " + Books.getPages());
+        Books books2 = new Books();
+        books2.setName("The Shining");
+        Books.setPages(447);
+        System.out.println(books2.getName() + ", " + Books.getPages());
         System.out.println();
-        System.out.println(books.flipPage(2,4));
+        System.out.println(books.flipPage(46));
+        System.out.println();
+        System.out.println(books.chapterListReturn(3));
+        System.out.println();
+        books.definingChapter(29);
         System.out.println();
 
         PostOffice post = new PostOffice();
-        post.setName("Новая почта");
-        post.setName2("Укрпочта");
-        post.setName3("Justin");
-        post.setClientsNewPost(1000000);
-        post.setClientsUkr(500000);
-        post.setClientsJustin(200000);
-        System.out.println(post.getName());
-        System.out.println(post.getClientsNewPost());
-        System.out.println(post.getName2());
-        System.out.println(post.getClientsUkr());
-        System.out.println(post.getName3());
-        System.out.println(post.getClientsJustin());
+        PostOffice.setName("New Poshta");
+        PostOffice.setClients(100000);
+        System.out.println(PostOffice.getName() + ", " + PostOffice.getClients() + ", " + post.getQUALIFICATION());
+        PostOffice.setName("UkrPochta");
+        PostOffice.setClients(10000);
+        System.out.println(PostOffice.getName() + ", " + PostOffice.getClients());
+        PostOffice.setName("Justin");
+        PostOffice.setClients(1000);
+        System.out.println(PostOffice.getName() + ", " + PostOffice.getClients());
         System.out.println();
-        System.out.println(PostOffice.calcMass(2,5,6));
+        System.out.println(PostOffice.calcMass(2, 4, 8));
+        System.out.println();
+        post.receivingList("Новая почта");
+        System.out.println();
+        PostOffice.packingShipping(3);
         System.out.println();
 
         Market supMarket = new Market();
-        supMarket.setName("Silpo");
-        supMarket.setName2("Varus");
-        supMarket.setName3("ATB");
-        supMarket.setClientsATB(100000);
-        supMarket.setClientsVarus(2000);
-        System.out.println(supMarket.getName());
-        System.out.println(supMarket.getClientsSilpo());
-        System.out.println(supMarket.getName2());
-        System.out.println(supMarket.getClientsVarus());
-        System.out.println(supMarket.getName3());
-        System.out.println(supMarket.getClientsATB());
-        System.out.println(supMarket.viewBonus(20));
+        Market.setName("ATB");
+        Market.setClients(100000);
+        System.out.println(Market.getDIRECTORS());
+        System.out.println(Market.getName() + ", " + Market.getClients());
+        Market.setName("Silpo");
+        Market.setClients(10000);
+        System.out.println(Market.getName() + ", " + Market.getClients());
+        supMarket.addBin("Умар");
+        System.out.println();
+        System.out.println(supMarket.viewBonus(10));
+        System.out.println();
+        System.out.println(supMarket.buyProduct(10000,"Наличка", "Кредитка"));
     }
 }
 
 class Books {
     private String name;
-    private String name2;
-    private String name3;
-    private static final String author = "STEPHEN_KING";
-    private static int pagesDark;
-    private static int pagesIt;
-    private static int pagesShining;
+    private static final String AUTHOR = "Stephen_King";
+    private static int pages;
+    int[] chapter = {1, 2, 3, 4};
 
-    public int flipPage(int a, int b) {
-        int page = 0;
-        if (a == 5) {
-            page += a;
-            System.out.println("Вы читаете " + page);
-        } else if (a < b) {
-            page += b;
-            System.out.println("Вы читаете " + page);
-        }
-        return page;
+    public int flipPage(int a) {
+        int input = a;
+        System.out.println("Вы читаете " + a + " страницу");
+        return pages = input;
     }
 
-    public int chapterListRet(int a, int b) {
-        a = 1;
-        b = 100;
+    public int chapterListReturn(int a) {
         if (a == 1) {
-            return a;
+            System.out.println("Вы читаете главу ");
+            return chapter[0];
+        } else if (a == 2) {
+            System.out.println("Вы читаете главу ");
+            return chapter[1];
+        } else if (a == 3) {
+            System.out.println("Вы читаете главу ");
+            return chapter[2];
         } else {
-            System.out.println("Перенажмите страницу");
-            return b;
+            System.out.println("Вы читаете главу ");
+            return chapter[3];
         }
     }
 
-    public int definingChapter(int a, int b, int c, int d) {
-        a = 10;
-        b = 20;
-        c = 30;
-        d = 40;
-        if (a == 0 && a <= 10) {
-            System.out.println("Вы читаете первую главу");
-        } else if (b > a && b <= c) {
-            System.out.println("Вы читаете вторую главу");
-        } else if (c > b && c <= d) {
-            System.out.println("Вы читаете третью главу");
+    public void definingChapter(int pages) {
+        if (pages >= 0 && pages <= 10) {
+            System.out.println("Глава определена как " + chapter[0]);
+        } else if (pages > 10 && pages <= 20) {
+            System.out.println("Глава определена как " + chapter[1]);
+        } else if (pages > 20 && pages <= 30) {
+            System.out.println("Глава определена как " + chapter[2]);
         } else {
-            System.out.println("Вы читаете четвертую главу");
+            System.out.println("Глава определена как " + chapter[3]);
         }
-        return a;
     }
 
     public String getName() {
@@ -188,79 +161,50 @@ class Books {
         this.name = name;
     }
 
-    public String getName2() {
-        return name2;
+    public static String getAUTHOR() {
+        return AUTHOR;
     }
 
-    public void setName2(String name2) {
-        this.name2 = name2;
+    public static int getPages() {
+        return pages;
     }
 
-    public String getName3() {
-        return name3;
-    }
-
-    public void setName3(String name3) {
-        this.name3 = name3;
-    }
-
-    public static String getAuthor() {
-        return author;
-    }
-
-    public static int getPagesDark() {
-        return pagesDark;
-    }
-
-    public static void setPagesDark(int pagesDark) {
-        Books.pagesDark = pagesDark;
-    }
-
-    public static int getPagesIt() {
-        return pagesIt;
-    }
-
-    public static void setPagesIt(int pagesIt) {
-        Books.pagesIt = pagesIt;
-    }
-
-    public static int getPagesShining() {
-        return pagesShining;
-    }
-
-    public static void setPagesShining(int pagesShining) {
-        Books.pagesShining = pagesShining;
+    public static void setPages(int pages) {
+        Books.pages = pages;
     }
 }
 
 class PostOffice {
     private static String name;
-    private static String name2;
-    private static String name3;
-    private static int clientsNewPost;
-    private static int clientsUkr;
-    private static int clientsJustin;
+    private static int clients;
+    final int QUALIFICATION = 100;
+    static int mass;
 
     public static int calcMass(int a, int b, int c) {
         System.out.println("Объёмный вес вашей посылки = кг ");
-        return a * b * c;
+        return mass = a * b * c;
     }
 
-    public int receivingList(int a, int b) {
-        return a + b;
-    }
-
-    public int packingShipping(int a, int b) {
-        if (a == 1) {
-            System.out.println("Ваша посылка весом 1 кг, отправка 10$");
-        } else if (a == 2) {
-            System.out.println("Ваша посылка больше 1 кг, отправка 10$ + 20$");
-            return a + b;
-        } else {
-            System.out.println("Ваша посылка тяжелая, заплатите пожалуйста 4 кратную сумму");
-            return a * b;
+    public void receivingList(String a) {
+        name = a;
+        if (name == "Новая почта") {
+            System.out.println("Ваша посылка будет получена немедленно");
+        } else if (name == "Укрпочта") {
+            System.out.println("Ваша посылка будет получена через 3-4 дня");
+        } else if (name == "Justin") {
+            System.out.println("Ваша посылка будет получена через сутки");
         }
-        return a + b;
+    }
+
+    public static void packingShipping(int a) {
+        mass = a;
+        if (mass == 1) {
+            System.out.println("Ваша посылка весом 1 кг, отправка 10$");
+        } else if (mass == 2) {
+            System.out.println("Ваша посылка больше 1 кг, отправка 10$ + 20$");
+        } else {
+            System.out.println("Ваша посылка тяжелая, заплатите пожалуйста 4-х кратную сумму");
+        }
     }
 
     public static String getName() {
@@ -271,71 +215,48 @@ class PostOffice {
         PostOffice.name = name;
     }
 
-    public static String getName2() {
-        return name2;
+    public static int getClients() {
+        return clients;
     }
 
-    public static void setName2(String name2) {
-        PostOffice.name2 = name2;
+    public static void setClients(int clients) {
+        PostOffice.clients = clients;
     }
 
-    public static String getName3() {
-        return name3;
-    }
-
-    public static void setName3(String name3) {
-        PostOffice.name3 = name3;
-    }
-
-    public static int getClientsNewPost() {
-        return clientsNewPost;
-    }
-
-    public static void setClientsNewPost(int clientsNewPost) {
-        PostOffice.clientsNewPost = clientsNewPost;
-    }
-
-    public static int getClientsUkr() {
-        return clientsUkr;
-    }
-
-    public static void setClientsUkr(int clientsUkr) {
-        PostOffice.clientsUkr = clientsUkr;
-    }
-
-    public static int getClientsJustin() {
-        return clientsJustin;
-    }
-
-    public static void setClientsJustin(int clientsJustin) {
-        PostOffice.clientsJustin = clientsJustin;
+    public int getQUALIFICATION() {
+        return QUALIFICATION;
     }
 }
 
 class Market {
     private static String name;
-    private static String name2;
-    private static String name3;
-    private static final int CLIENTS_SILPO = 10000;
-    private static int clientsVarus;
-    private int clientsATB;
+    private static final int DIRECTORS = 2;
+    private static int clients;
+    int bonus;
+    public int money;
 
-    public void addBin() {
-        System.out.println("Возьмите корзину с полки и наполняйте её по мере необходимости товарами");
+    public void addBin(String a) {
+        name = a;
+        if (name == "Умар") {
+            System.out.println("Возьмите пожалуйста корзину с полки и наполняйте её " +
+                    "по мере необходимости товарами");
+        } else {
+            System.out.println("Возьми пакет и клади туда не больше 2-х вещей");
+        }
     }
 
     public int viewBonus(int a) {
-        System.out.println("У вас " + a + " накопленных бонусов");
-        return a * a;
+        bonus = a;
+        System.out.println("У вас " + bonus + " накопленных бонусов, эта сумма возведётся в квадрат!");
+        return bonus * a;
     }
 
-    public String buyProduct(String cart, String cash) {
-        int a = 10;
-        int b = 100;
-        if (a <= 10) {
-            return cash;
-        } else {
-            return cart;
+    public String buyProduct(int cash, String nalichka, String carta) {
+        money = cash;
+        if(cash <= 1000){
+            return nalichka;
+        }else {
+            return carta;
         }
     }
 
@@ -347,39 +268,15 @@ class Market {
         Market.name = name;
     }
 
-    public static String getName2() {
-        return name2;
+    public static int getDIRECTORS() {
+        return DIRECTORS;
     }
 
-    public static void setName2(String name2) {
-        Market.name2 = name2;
+    public static int getClients() {
+        return clients;
     }
 
-    public static String getName3() {
-        return name3;
-    }
-
-    public static void setName3(String name3) {
-        Market.name3 = name3;
-    }
-
-    public static int getClientsSilpo() {
-        return CLIENTS_SILPO;
-    }
-
-    public static int getClientsVarus() {
-        return clientsVarus;
-    }
-
-    public static void setClientsVarus(int clientsVarus) {
-        Market.clientsVarus = clientsVarus;
-    }
-
-    public int getClientsATB() {
-        return clientsATB;
-    }
-
-    public void setClientsATB(int clientsATB) {
-        this.clientsATB = clientsATB;
+    public static void setClients(int clients) {
+        Market.clients = clients;
     }
 }
